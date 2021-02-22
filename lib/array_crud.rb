5fc4181826254d9a28654622707a5be05024511b
Diana Liao <dijali@gmail.com>
2021-02-22 10:59:58 -0500

    commit tree b273da9c13e286b3b10923b02dd2d58d953339fe

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["bagel","cookie","tea","dumplings"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def add_element_to_start_of_array(array, element)
